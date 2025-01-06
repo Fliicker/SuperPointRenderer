5183171910c028b8dbebd62d8644a8e2e9eb9578
fliicker <723833066@qq.com>
2025-01-06 21:42:11 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MapView from "./components/MapView.vue";
+</script>
+
+<template>
+  <MapView />
+</template>
+
+<style>
+@import "assets/base.css";
+</style>
---
+++ src/components/MapView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="map-container" ref="mapContainer"></div>
+</template>
+
+<script setup>
+import { ref, reactive, onMounted } from "vue";
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZmxpY2tlcjA1NiIsImEiOiJjbGd4OXM1c3cwOWs3M21ta2RiMDhoczVnIn0.lE8NriBf_g3RZWCusw_mZA";
+
+const mapContainer = ref(null);
+let map;
+
+onMounted(async () => {
+  map = new mapboxgl.Map({
+    container: "map-container",
+    center: [118.800697, 32.064162], // starting position [lng, lat]
+    zoom: 4,
+    style: "mapbox://styles/mapbox/satellite-v9", // style URL
+  });
+});
+</script>
+
+<style lang="scss">
+#map-container {
+  width: 100%;
+  height: 100%;
+}
+</style>
